c4165b87ad98366e6ea10a952995d518c33823e5
dmitry <d.kozhinov@mail.ru>
2020-04-24 10:12:04 +0500

    Исправлена досадная ошибка в beh2cuc.py

--- cerber-ui/beh2cuc.py
+++ cerber-ui/beh2cuc.py
@@ -3,7 +3,6 @@ import json
 import os
 
 if __name__ == '__main__':
-if __name__ == '__main__':
     with open(os.path.abspath('./reports/behave.json')) as behave_json_file:
         with open(os.path.abspath('./reports/cucu.json'), 'w+') as cucumber_json_file:
             cucumber_json = behave2cucumber.convert(json.load(behave_json_file))
